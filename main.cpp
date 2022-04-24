7695c68dcddaae1765ac00ed397b928c8f60a309
jayrountree <jaynr22@gmail.com>
2022-04-24 00:55:44 -0500

    added instructions

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 using namespace std;
 
 int main () {
-    cout << "Welcome to Addle. Each turn, a new letter will be added to create a new guessable word.\n";
-    cout << "This new letter will be denoted by a \"^\", and letters will continue to be added until a seven letter word is reached.\n";
+    cout << "Welcome to Addle. Each turn, a new letter will be inserted to create a new guessable word.\n";
+    cout << "This new letter will be denoted by a \"^\", and new letters will continue to be inserted until seven letters are reached.\n";
     cout << "Try and guess the word as quick as you can. Good luck!\n";
     playGame game = playGame("wordsList.txt");
     string guess;
     while(!game.hasWon()) {
         game.playTurn();
     }
-    cout << "Congrats! You have beaten Addle.";
+    cout << "Congrats! You have beaten Addle in " << game.getTurn() << " tries.\n";
+    cout << "Here is every word in this Addle:\n";
+    game.displayWords();
 }
